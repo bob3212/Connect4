ed9d507c7457e36eac290a0f575c71191dd283fc
bob3212 <yudavid17@gmail.com>
2020-11-13 18:20:07 -0500

    updated user schema

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -29,11 +29,15 @@ class Search extends React.Component {
         return axios.get(`${url}/users/search/${username}`)
     }
 
+    addFriend = (userId) => {
+
+    }
+
     showUsers = () => {
         return (
             <Table striped bordered hover variant="dark">
                 <tbody>
-                    {this.state.results.data && this.state.results.data.map(result => <tr onClick={(e) => window.location.href = `/UserProfile/${result._id}`}><th>{result.username}</th></tr>)}
+                    {this.state.results.data && this.state.results.data.map(result => <tr onClick={(e) => window.location.href = `/UserProfile/${result._id}`}><th>{result.username}</th><th onClick={this.addFriend(result._id)}>Add Friend</th></tr>)}
                 </tbody>
             </Table>
         )
